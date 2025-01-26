3364758fb479c9f1413024196fd01930c5af60fe
hakimtaman <hakimtaman@gmail.com>
2025-01-26 17:24:00 +0200

    Updates

---
+++ precache-manifest.0d953366d11fe5c57efb484af9fa8a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3247043e5fd7f891401916c451e0a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "336d8096d72e49ebb6c2",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "94274bfaa0cd7521ee35",
+    "url": "/static/css/main.53a4070c.chunk.css"
+  },
+  {
+    "revision": "336d8096d72e49ebb6c2",
+    "url": "/static/js/2.e210018d.chunk.js"
+  },
+  {
+    "revision": "94274bfaa0cd7521ee35",
+    "url": "/static/js/main.b46fa346.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
